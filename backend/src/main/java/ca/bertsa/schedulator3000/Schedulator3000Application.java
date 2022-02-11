2ec0830b743539cea42cc5a9e58cc17633d10619
Samuel Bertrand <bertsa.pro@gmail.com>
2022-02-11 13:08:15 -0500

    Init

---
+++ backend/src/main/java/ca/bertsa/schedulator3000/Schedulator3000Application.java
@@ -0,0 +1,13 @@
+package ca.bertsa.schedulator3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schedulator3000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schedulator3000Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/ca/bertsa/schedulator3000/Schedulator3000ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bertsa.schedulator3000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Schedulator3000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
